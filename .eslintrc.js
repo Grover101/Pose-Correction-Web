e64ddcbd2a71b171398a35e78d7b7e540e8b8dde
Grover101 <limachigrover09@gmail.com>
2022-10-31 12:22:33 -0400

    add config de indentacion

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+const RULES = {
+    OFF: 'off',
+    WARN: 'warn',
+    ERROR: 'error'
+}
+
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+        node: true
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'standard',
+        'prettier'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 11,
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': RULES.OFF,
+        'react/react-in-jsx-scope': RULES.OFF
+    }
+}
